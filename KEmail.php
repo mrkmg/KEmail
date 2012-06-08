142202966a8085286006280cc0dfce217d7fbdbb
Kevin Gravier <kgravier@fluctuationsoftware.com>
2012-06-08 10:12:05 -0400

    Added Doc's to KEmail

--- KEmail.php
+++ KEmail.php
@@ -1,34 +1,138 @@
 <?php
 
-
-
+/**
+ * KEmail is an Yii Application Component that provides direct smtp interaction
+ *
+ * KEmail wraps http://www.phpclasses.org/package/14-PHP-Sends-e-mail-messages-via-SMTP-protocol.html to
+ * provide a simple way to send email's to specific smtp servers from within php only. Does not require
+ * php's mail() or any other library.
+ *
+ * @package KEmail
+*/
 class KEmail extends CApplicationComponent
 {
+    /**
+     * @var string Host name of smtp server
+    */
     public $host_name="localhost";
+    
+    /**
+     * @var integer Port of smtp server
+    */
     public $host_port=25;
+    
+    /**
+     * @var bool Force SSL
+    */
     public $ssl=false;
+    
+    /**
+     * @var string Set to enable using an http proxy to access smtp server
+    */
     public $http_proxy_host_name='';
+    
+    /**
+     * @var integer Port of http proxy
+    */
     public $http_proxy_host_port=3128;
+    
+    /**
+     * @var string Set to enable using a socks proxy
+    */
     public $socks_host_name='';
+    
+    /**
+     * @var integer Port of socks proxy
+    */
     public $socks_host_port=1080;
+    
+    /**
+     * @var integer Version of socks proxy
+    */
     public $socks_version='5';
+    
+    /**
+     * @var bool Force `start_tls`
+    */
     public $start_tls=false;
+    
+    /**
+     * @var string Given hostname of client
+    */
     public $localhost='localhost';
+    
+    /**
+     * @var bool Skip smtp server and delevier directly to recipients smtp
+    */
     public $direct_delivery=false;
+    
+    /**
+     * @var integer Time in seconds to timeout for all smtp connections
+    */
     public $timeout=10;
+    
+    /**
+     * @var integer Time in seconds to timeout for data transfer to SMTP server, if 0 uses timeout
+    */
     public $data_timeout=0;
+    
+    /**
+     * @var bool Output Debug information to browser
+    */
     public $debug=false;
+    
+    /**
+     * @var bool Format Debug information as html, caution as this does not care when it is called, it will immediatly output
+    */
     public $html_debug=true;
+    
+    /**
+     * @var string Set to authenticate to a pop3 server
+    */
     public $pop3_auth_host='';
+    
+    /**
+     * @var string Username for smtp authentication
+    */
     public $user="";
+    
+    /**
+     * @var string Realm for smtp authentication
+    */
     public $realm="";
+    
+    /**
+     * @var string Password for smtp authentication
+    */
     public $password="";
+    
+    /**
+     * @var string Workstation for smtp authentication
+    */
     public $workstation="";
+    
+    /**
+     * @var string Force a specific smtp authentication mechanism ('LOGIN','PLAIN','CRAM-MD5','NTLM')
+    */
     public $authentication_mechanism="";
     
+    /**
+     * @var string current directory of this script file, used to include required libraries
+     * @access private
+    */
     private $current_directory;
+    
+    /**
+     * @var smtp_class Holder for library class
+    */
     private $smtp_object;
     
+    
+    /**
+     * Imports required libraries and sets configuration
+     *
+     * @access public
+    */
     public function init()
     {
         $this->current_directory = dirname(__FILE__).DIRECTORY_SEPARATOR;
@@ -78,6 +182,19 @@ class KEmail extends CApplicationComponent
         }
     }
     
+    /**
+     * Sends an email via smtp library
+     *
+     * @param string $from From email address
+     * @param array|string $to To email address(es)
+     * @param string $subject Subject of email
+     * @param string $body Body of the email
+     * @param array $additional_headers More headers to include in the email
+     *
+     * @return bool Whether or not the email was sent
+     *
+     * @throws Exception when $to is not an array or string
+    */
     public function send($from,$to,$subject,$body,$additional_headers=array())
     {
         $to_f = array();
